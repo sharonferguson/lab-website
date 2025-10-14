8690344a711ad0c0e25b951946be1edeb6cb73d9
sharonferguson <52184755+sharonferguson@users.noreply.github.com>
2025-10-14 00:09:24 +0000

    Deploying to gh-pages from @ sharonferguson/lab-website@ec5ba4546f154ac1fa244a33b49c2c322aeb14f2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -413,9 +413,9 @@ ninja.data = [{
           description: "AI-enabled support for human-human teamwork",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-flexible-working-configuration",
+          title: 'Flexible Working Configuration',
+          description: "Studying how collaboration norms change in the modern workplace",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
