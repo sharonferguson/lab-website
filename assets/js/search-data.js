27d2a26f199d88b29fddb11b88c60a9eb5ba6705
sharonferguson <52184755+sharonferguson@users.noreply.github.com>
2025-10-14 01:16:14 +0000

    Deploying to gh-pages from @ sharonferguson/lab-website@7389526403a84af7aad29fbc3bc56050dc6a589d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,35 +419,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%73%68%61%72%6F%6E.%66%65%72%67%75%73%6F%6E@%75%77%61%74%65%72%6C%6F%6F.%63%61", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=TXXTPIkAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
