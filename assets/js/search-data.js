7f52b1c73729fb1a50dda3ca26d79e64b709202f
keenanung <87099558+keenanung@users.noreply.github.com>
2026-05-23 17:55:35 +0000

    Deploying to gh-pages from @ sharonferguson/lab-website@eb6b8ac425170a23bb0937afdeef8489be764409 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,27 +4,27 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "A broad overview of the SHARE Lab areas of research",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
           },
         },{id: "nav-people",
-          title: "people",
+          title: "People",
           description: "Current and past members and collaborators of SHARE Lab",
           section: "Navigation",
           handler: () => {
